c0f009032ddb70edca7f36f8546e52bf77313885
Vishal Sharma <sharma.vishalawake@gmail.com>
2023-03-25 22:51:30 +0100

    Hello World commit

---
+++ hello.pl
@@ -0,0 +1,11 @@
+print "Hello World \n" ;
+
+$item = 5;
+
+@item_list = ( 1,2,3,4) ;
+
+%item_choice = ( "Audi",1,"BMW",2,"Merc",3) ;
+
+print "The item value is $item \n";
+print "The item list is @item_list \n" ;
+print "The item choice list is $item_choice{'Audi'} \n";
